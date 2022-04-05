f3b7558ffc625262ac6f4f39f7c6785c1c8ac924
rmonteroh <rmonteroh90@gmail.com>
2022-04-05 02:51:10 -0500

    test

--- components/views/HomePage.tsx
+++ components/views/HomePage.tsx
@@ -49,6 +49,8 @@ const HomePage = () => {
       if (observer.current) observer.current.disconnect();
       observer.current = new IntersectionObserver((entries) => {
         if (entries[0].isIntersecting && hasMore) {
+          console.log('intersectiong');
+          
           searchProperty(true);
         }
       });
